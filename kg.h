60a5b8cea504ee40ac308fa1a6105a09e7f55027
Nikita-Babich <nikita.babich.a@gmail.com>
2026-07-19 18:18:53 +0300

    Puzzle environment 1

--- kg.h
+++ kg.h
@@ -1,15 +1,24 @@
-//	KG header-only library
+//	KG  - header-only library
 // Helper functions, recursively added headers, low level implementations, puzzle instruments
 
-
+// Segment for FU - FREQUENTLY USED
 #include <stdarg.h>
 
 #include <stdio.h>
 
 #define print(arg) printf("%s = %lld \n", #arg, arg)
 
-// PE - puzzle environment
+// Segment for PE - puzzle environment
 #define natural long long int
+int kg_pe_DigitAmount(natural n){
+	
+}
 int kg_pe_GrowingNumberQ(natural n ){
 	return 14;
+}
+int kg_pe_DescendingNumberQ(natural n ){
+	return 14;
+}
+inline int kg_pe_BouncyNumberQ(natural n){
+	return ( !kg_pe_GrowingNumberQ( n ) ) && ( !kg_pe_DescendingNumberQ( n ) )
 }
