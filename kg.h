53d5951a021442c7ea096da7693e386ec6e42d8a
Nikita-Babich <nikita.babich.a@gmail.com>
2026-07-19 18:45:26 +0300

    Update kg.h

--- kg.h
+++ kg.h
@@ -4,21 +4,30 @@
 // Segment for FU - FREQUENTLY USED
 #include <stdarg.h>
 
+#include <math.h>
 #include <stdio.h>
 
-#define print(arg) printf("%s = %lld \n", #arg, arg)
+#define print(arg) printf("%s = %lld \n", #arg, (long long int)arg)
+//#define print2(arg) printf(#arg " = %lld \n",  arg)
 
 // Segment for PE - puzzle environment
 #define natural long long int
 int kg_pe_DigitAmount(natural n){
-	
+	return (int)log10((double) n) + 1;
+}
+int kg_pe_TakeDigit(natural n, int index){
+	// Todo
 }
 int kg_pe_GrowingNumberQ(natural n ){
+	int len = kg_pe_DigitAmount(n);
+	for (int i = 1; i<=len; i++){
+		
+	}
 	return 14;
 }
 int kg_pe_DescendingNumberQ(natural n ){
 	return 14;
 }
 inline int kg_pe_BouncyNumberQ(natural n){
-	return ( !kg_pe_GrowingNumberQ( n ) ) && ( !kg_pe_DescendingNumberQ( n ) )
+	return ( !kg_pe_GrowingNumberQ( n ) ) && ( !kg_pe_DescendingNumberQ( n ) );
 }
