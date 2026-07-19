6c1825fd821507c3be2bc913db13fc8a6ec2cb24
Nikita-Babich <nikita.babich.a@gmail.com>
2026-07-19 18:08:22 +0300

    starting building convenience header

---
+++ kg.h
@@ -0,0 +1,15 @@
+//	KG header-only library
+// Helper functions, recursively added headers, low level implementations, puzzle instruments
+
+
+#include <stdarg.h>
+
+#include <stdio.h>
+
+#define print(arg) printf("%s = %lld \n", #arg, arg)
+
+// PE - puzzle environment
+#define natural long long int
+int kg_pe_GrowingNumberQ(natural n ){
+	return 14;
+}
